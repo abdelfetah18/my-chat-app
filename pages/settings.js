9fd96dd5dbdeceee77581b6f4476538f642b4fdc
Abdelfetah <nstart0319@gmail.com>
2022-07-29 18:38:09 +0100

    login loading!

--- pages/settings.js
+++ pages/settings.js
@@ -1,7 +1,16 @@
 import Navigation from "../components/Navigation";
 import { FaBell } from 'react-icons/fa';
+import { Cookies } from "react-cookie";
 
 export default function Settings(){
+    var cookies = new Cookies();
+
+    function Logout(){
+        localStorage.clear();
+        cookies.remove('access_token');
+        window.location.href = "/sign_in";
+    }
+    
     return(
         <div className='flex flex-row background h-screen w-screen'>
             <Navigation page={'/settings'} />
@@ -12,6 +21,12 @@ export default function Settings(){
                 </div>
                 <div className='w-full text-start font-mono font-bold text-2xl px-4 py-2 text-[#02166c]'>Settings</div>
                 <div className='flex flex-row w-full'>
+                    <div className="flex flex-col items-center w-1/2">
+                        <div className="w-11/12 font-mono text-lg font-semibold text-[#02166c]">Profile</div>
+                        <div className="cw-11/12 flex flex-col items-center my-2">
+                            <div onClick={Logout} className="cursor-pointer px-4 py-1 bg-blue-500 rounded-lg font-semibold text-white w-fit">Logout</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- pages/sign_in.js
+++ pages/sign_in.js
@@ -8,37 +8,44 @@ import { useCookies } from "react-cookie";
 export default function SignIn(){
     const [username,setUsername] = useState('');
     const [password,setPassword] = useState('');
+    const [is_loading,setIsLoading] = useState(false);
     const [cookies, setCookie, removeCookie] = useCookies();
     var alertBoxAnimation = useAnimation();
     var alertBox = useRef();
 
     function sign_in(evt){
-        axios.post('/api/v1/sign_in',{
-            username,password
-        }).then((response) => {
-            console.log('res:',response.data);
-            console.log("response:",response);
-            if(response.data.status === 'success'){
-                localStorage.setItem('access_token',response.data.token);
-                setCookie("access_token",response.data.token);
-                alertBox.current.innerText = response.data.message;
-                alertBoxAnimation.start({
-                    opacity:1,
-                    backgroundColor:'#7ded4e'
-                },{
-                    duration:0.5
-                });
-                location.href = '/'
-            }else{
-                alertBox.current.innerText = response.data.message;
-                alertBoxAnimation.start({
-                    opacity:1,
-                    backgroundColor:'#ff4343'
-                },{
-                    duration:0.5
-                })
-            }
-        }).catch((err) => console.log('error:',err));
+        if(!is_loading){
+            setIsLoading(true);
+            axios.post('/api/v1/sign_in',{
+                username,password
+            }).then((response) => {
+                setIsLoading(false);
+                console.log("response:",response);
+                if(response.data.status === 'success'){
+                    localStorage.setItem('access_token',response.data.token);
+                    setCookie("access_token",response.data.token);
+                    alertBox.current.innerText = response.data.message;
+                    alertBoxAnimation.start({
+                        opacity:1,
+                        backgroundColor:'#7ded4e'
+                    },{
+                        duration:0.5
+                    });
+                    location.href = '/'
+                }else{
+                    alertBox.current.innerText = response.data.message;
+                    alertBoxAnimation.start({
+                        opacity:1,
+                        backgroundColor:'#ff4343'
+                    },{
+                        duration:0.5
+                    })
+                }
+            }).catch((err) => {
+                setIsLoading(false);
+                console.log('error:',err);
+            });
+        }
     }
 
     return(
@@ -61,6 +68,19 @@ export default function SignIn(){
                     </div>
                 </div>
             </div>
+            {
+                is_loading ? (
+                    <div className="absolute flex flex-col items-center justify-center h-screen w-screen bg-[#00000099]">
+                        <div role="status">
+                            <svg aria-hidden="true" class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"></path>
+                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"></path>
+                            </svg>
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                    </div>
+                ) : ('')
+            }
         </div>
     )
 }
