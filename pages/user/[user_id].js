8290771c57d84a5fce0d23369ca87e50ef12dc7a
abdelfetah18 <nstart0319@gmail.com>
2023-09-12 18:52:46 +0100

    Fixing UI issue

--- pages/user/[user_id].js
+++ pages/user/[user_id].js
@@ -27,14 +27,14 @@ export default function UserProfile({ user }){
         <div className='flex flex-col sm:flex-row background h-screen w-screen'>
             <Navigation page={'/'} />
             <div className='h-full lg:w-5/6 md:w-11/12 w-full bg-[#f1f5fe] sm:rounded-l-3xl flex flex-col px-10 py-4'>
-                <div className='w-full flex flex-row justify-end items-center px-4 py-2'>
+                <div className='w-full flex flex-row justify-end items-center py-2'>
                     <FaBell className='font-mono text-[#ccd8e8] mx-1 text-base' />
                     <div className='font-mono text-[#9199a8] mx-1 text-sm'>Notifications</div>
                 </div>
-                <div className='w-full text-start font-mono font-bold text-2xl px-4 py-2 text-[#02166c]'>User profile</div>
+                <div className='w-full text-start font-mono font-bold text-2xl py-2 text-[#02166c]'>User profile</div>
                 <div className='flex flex-col sm:flex-row w-full h-full overflow-auto'>
-                    <div className="w-11/12 sm:w-2/3 flex flex-col items-center flex-grow overflow-auto">
-                        <div className="bg-gray-50 rounded-lg w-11/12 flex flex-col items-center">
+                    <div className="w-full sm:w-2/3 flex flex-col items-center flex-grow overflow-auto">
+                        <div className="bg-gray-50 rounded-lg w-full flex flex-col items-center">
                             <div className="w-full h-40 rounded-lg bg-gray-200 shadow-lg">
                                 <img className="w-full h-full object-cover rounded-lg" src={User.cover_image || "/cover.png"} />
                             </div>
