ae268c64eac5fc6e39793b6f9d2234f3ea9229a1
abdelfetah18 <nstart0319@gmail.com>
2026-02-21 01:33:57 +0100

    Improve create room UI

--- pages/create_room.tsx
+++ pages/create_room.tsx
@@ -13,10 +13,9 @@ export default function CreateRoom() {
     const selectedStep = steps[currentStepIndex];
     const sliderContainerRef = useRef<HTMLDivElement>(null);
     const toastManager = useContext(ToastContext);
-    let { room, setRoom, createRoom, upload_profile_image, upload_cover_image } = useRoom();
+    let { room, setRoom, createRoom, upload_profile_image } = useRoom();
 
     const profileImageInputRef = useRef<HTMLInputElement>(null);
-    const coverImageInputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         if (sliderContainerRef.current) {
@@ -53,19 +52,16 @@ export default function CreateRoom() {
         if (profileImageInputRef.current.value) {
             await upload_profile_image(room._id, profileImageInputRef.current.files[0]);
         }
-        if (coverImageInputRef.current.value) {
-            await upload_cover_image(room._id, coverImageInputRef.current.files[0]);
-        }
         setIsLoading(false);
         toastManager.alertSuccess("Room created successfully.");
         setTimeout(() => { router.push(`/rooms/${room._id}`); }, 500);
     }
 
     return (
-        <div className="w-full h-full flex flex-col items-center justify-center">
-            <div className='text-3xl font-semibold my-10'>Create Room</div>
-            <div className='w-11/12 px-10 py-6 flex bg-white rounded-xl'>
-                <div className='w-2/5 flex flex-col'>
+        <div className="w-full h-full flex flex-col items-center py-6 overflow-auto">
+            <div className='w-11/12 h-full px-10 py-6 flex flex-col gap-8 bg-white rounded-xl'>
+                <div className='text-2xl font-semibold'>Create Room</div>
+                <div className='w-full flex flex-col'>
                     <div className='w-full flex items-center gap-4 py-4'>
                         {
                             steps.map((step, index) => {
@@ -99,10 +95,10 @@ export default function CreateRoom() {
                         <div className='text-xs text-gray-400 max-w-sm'>{selectedStep.description}</div>
                     </div>
                 </div>
-                <div className='w-3/5 flex flex-col'>
-                    <div ref={sliderContainerRef} className='w-full flex overflow-hidden'>
+                <div className='w-full flex-grow flex flex-col'>
+                    <div ref={sliderContainerRef} className='w-full flex-grow flex overflow-hidden'>
                         <SliderWrapper selected={selectedStep.slug == "Room Details"}><RoomDetails useRoom={[room, setRoom]} /></SliderWrapper>
-                        <SliderWrapper selected={selectedStep.slug == "Room Appearance"}><RoomApearance useRoom={[room, setRoom]} profileImageInputRef={profileImageInputRef} coverImageInputRef={coverImageInputRef} /></SliderWrapper>
+                        <SliderWrapper selected={selectedStep.slug == "Room Appearance"}><RoomApearance useRoom={[room, setRoom]} profileImageInputRef={profileImageInputRef} /></SliderWrapper>
                         <SliderWrapper selected={selectedStep.slug == "Room Visibility"}><RoomVisibility useRoom={[room, setRoom]} /></SliderWrapper>
                         <SliderWrapper selected={selectedStep.slug == "Review and Create"}><RoomReview room={room} /></SliderWrapper>
                     </div>
@@ -156,9 +152,17 @@ function RoomDetails({ useRoom }: RoomDetailsProps) {
     const [room, setRoom] = useRoom;
 
     return (
-        <div className='w-full flex flex-col gap-2'>
-            <input value={room.name} onChange={(ev) => setRoom(state => ({ ...state, name: ev.target.value }))} className='bg-gray-100 rounded-full py-2 px-4 text-sm outline-none' type='text' placeholder='Enter Name' />
-            <textarea value={room.bio} onChange={(ev) => setRoom(state => ({ ...state, bio: ev.target.value }))} className='bg-gray-100 rounded-xl py-2 px-4 text-sm outline-none' placeholder='Enter Description or bio' rows={6} />
+        <div className='w-full grid grid-cols-3 gap-2 gap-y-8'>
+            <div className='w-full flex flex-col'>
+                <div>Name</div>
+                <div className='text-sm text-zinc-500'>A unique name that tells users what the room is about.</div>
+            </div>
+            <input value={room.name} onChange={(ev) => setRoom(state => ({ ...state, name: ev.target.value }))} className='col-span-2 bg-gray-100 rounded-full py-2 px-4 text-sm outline-none' type='text' placeholder='Enter Name' />
+            <div className='w-full flex flex-col'>
+                <div>Description</div>
+                <div className='text-sm text-zinc-500'>A short summary that explains what the room is about.</div>
+            </div>
+            <textarea value={room.bio} onChange={(ev) => setRoom(state => ({ ...state, bio: ev.target.value }))} className='col-span-2 bg-gray-100 rounded-xl py-2 px-4 text-sm outline-none' placeholder='Enter Description or bio' rows={6} />
         </div>
     )
 }
@@ -166,10 +170,9 @@ function RoomDetails({ useRoom }: RoomDetailsProps) {
 interface RoomApearanceProps {
     useRoom: [Room, React.Dispatch<React.SetStateAction<Room>>];
     profileImageInputRef: React.MutableRefObject<HTMLInputElement>;
-    coverImageInputRef: React.MutableRefObject<HTMLInputElement>;
 };
 
-function RoomApearance({ useRoom, profileImageInputRef, coverImageInputRef }: RoomApearanceProps) {
+function RoomApearance({ useRoom, profileImageInputRef }: RoomApearanceProps) {
     const [room, setRoom] = useRoom;
 
     function readProfileImageURL(input: HTMLInputElement): void {
@@ -184,46 +187,29 @@ function RoomApearance({ useRoom, profileImageInputRef, coverImageInputRef }: Ro
         }
     }
 
-    function readCoverImageURL(input: HTMLInputElement): void {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                setRoom(state => ({ ...state, cover_image: { url: e.target.result.toString() } }));
-            }
-
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-
     function onSelectProfileImage(ev: ChangeEvent<HTMLInputElement>): void {
         readProfileImageURL(ev.target);
     }
 
-    function onSelectCoverImage(ev: ChangeEvent<HTMLInputElement>): void {
-        readCoverImageURL(ev.target);
-    }
-
     return (
-        <div className='w-full flex flex-col relative'>
+        <div className='w-full grid grid-cols-3 gap-2 gap-y-8'>
             <input ref={profileImageInputRef} onChange={onSelectProfileImage} accept='image/*' type='file' hidden />
-            <input ref={coverImageInputRef} onChange={onSelectCoverImage} accept='image/*' type='file' hidden />
-            <div className='w-full aspect-[4/1] bg-gray-200 rounded-xl relative'>
-                <img src={(room.cover_image ? room.cover_image.url : '/cover.png')} className='absolute top-0 left-0 w-full h-full object-cover rounded-xl' />
-                <div onClick={() => coverImageInputRef.current.click()} className='absolute bottom-2 right-2 bg-secondaryColor text-white px-8 py-1 text-sm rounded-full flex items-center gap-2 cursor-pointer active:scale-105 duration-300 select-none hover:bg-primaryColor'>
-                    <FaImage />
-                    <span>Cover Image</span>
-                </div>
+
+            <div className='w-full flex flex-col'>
+                <div>Profile Image</div>
+                <div className='text-sm text-zinc-500'>Upload a picture to represent this room.</div>
             </div>
-            <div className='absolute left-1/2 -translate-x-1/2 bottom-0 h-28 w-28 bg-gray-300 rounded-full'>
-                <div className='w-full h-full relative'>
-                    <img src={(room.profile_image ? room.profile_image.url : '/profile.png')} className='absolute top-0 left-0 w-full h-full rounded-full object-cover' />
-                    <div onClick={() => profileImageInputRef.current.click()} className='absolute bottom-1 right-1 bg-secondaryColor text-white p-2 text-sm rounded-full flex items-center gap-2 cursor-pointer active:scale-105 duration-300 select-none hover:bg-primaryColor'>
-                        <FaImage />
+            <div className='col-span-2 w-full'>
+                <div className='w-full'>
+                    <div className='w-full flex flex-row items-center gap-4'>
+                        <img src={(room.profile_image ? room.profile_image.url : '/profile.png')} className='w-28 h-28 rounded-full object-cover' />
+                        <div onClick={() => profileImageInputRef.current.click()} className='bg-secondaryColor text-white p-2 text-sm rounded-full flex items-center gap-2 cursor-pointer active:scale-105 duration-300 select-none hover:bg-primaryColor'>
+                            <FaImage />
+                            <div>Upload profile image</div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className='h-8 w-full'></div>
         </div>
     )
 }
@@ -235,16 +221,28 @@ function RoomVisibility({ useRoom }: RoomVisibilityProps) {
     const [room, setRoom] = useRoom;
 
     return (
-        <div className='w-full flex flex-col gap-4'>
-            <select value={room.is_public ? "public" : "private"} onChange={(ev) => setRoom(state => ({ ...state, is_public: ev.target.value == "public" }))} className='bg-gray-100 rounded-full py-2 px-4 text-sm border-r-8 border-r-gray-100 outline-none'>
+        <div className='w-full grid grid-cols-3 gap-2 gap-y-8'>
+            <div className='w-full flex flex-col'>
+                <div>Visibility</div>
+                <div className='text-sm text-zinc-500'>Controls who can find and join this room.</div>
+            </div>
+            <select value={room.is_public ? "public" : "private"} onChange={(ev) => setRoom(state => ({ ...state, is_public: ev.target.value == "public" }))} className='col-span-2 bg-gray-100 rounded-full py-2 px-4 text-sm border-r-8 border-r-gray-100 outline-none'>
                 <option disabled>Select Visibility</option>
                 <option value="public">Public</option>
                 <option value="private">Private</option>
             </select>
-            <div className='w-full flex flex-col gap-2'>
-                <div className='text-sm w-full'>Password:</div>
-                <input value={room.is_public ? "" : room.password} onChange={(ev) => setRoom(state => ({ ...state, password: ev.target.value }))} className={`w-full bg-gray-100 rounded-full py-2 px-4 text-sm outline-none ${room.is_public ? "opacity-80 placeholder:text-gray-300" : ""}`} type='password' placeholder={room.is_public ? "Public Rooms Don't have a Password" : "Enter Password"} disabled={room.is_public} />
-            </div>
+            {!room.is_public && (
+                <>
+                    <div className='w-full flex flex-col'>
+                        <div>Password</div>
+                        <div className='text-sm text-zinc-500'>A required code that users must enter to access the room.</div>
+                    </div>
+                    <div className='col-span-2 w-full flex flex-col gap-2'>
+                        <div className='text-sm w-full'>Password:</div>
+                        <input value={room.is_public ? "" : room.password} onChange={(ev) => setRoom(state => ({ ...state, password: ev.target.value }))} className={`w-full bg-gray-100 rounded-full py-2 px-4 text-sm outline-none ${room.is_public ? "opacity-80 placeholder:text-gray-300" : ""}`} type='password' placeholder={room.is_public ? "Public Rooms Don't have a Password" : "Enter Password"} disabled={room.is_public} />
+                    </div>
+                </>
+            )}
         </div>
     )
 }
@@ -254,28 +252,30 @@ interface RoomReviewProps {
 };
 function RoomReview({ room }: RoomReviewProps) {
     return (
-        <div className='w-full flex flex-col'>
-            <div className='w-full flex flex-col relative'>
-                <div className='w-full aspect-[4/1] bg-gray-200 rounded-xl relative'>
-                    <img src={(room.cover_image ? room.cover_image.url : '/cover.png')} className='absolute top-0 left-0 w-full h-full object-cover rounded-xl' />
-                </div>
-                <div className='absolute left-4 bottom-0 h-28 w-28 bg-gray-300 rounded-full'>
-                    <div className='w-full h-full relative'>
-                        <img src={(room.profile_image ? room.profile_image.url : '/profile.png')} className='absolute top-0 left-0 w-full h-full rounded-full object-cover' />
+        <div className='w-full flex flex-col items-center'>
+            <div className='w-full flex flex-col items-center'>
+                <div className='w-full flex flex-col relative'>
+                    <div className='w-full aspect-[4/1] bg-gray-200 rounded-xl relative'>
+                        <img src={(room.cover_image ? room.cover_image.url : '/cover.png')} className='absolute top-0 left-0 w-full h-full object-cover rounded-xl' />
                     </div>
+                    <div className='absolute left-1/2 -translate-x-1/2 bottom-0 h-28 w-28 bg-gray-300 rounded-full'>
+                        <div className='w-full h-full relative'>
+                            <img src={(room.profile_image ? room.profile_image.url : '/profile.png')} className='absolute top-0 left-0 w-full h-full rounded-full object-cover' />
+                        </div>
+                    </div>
+                    <div className='h-8 w-full'></div>
                 </div>
-                <div className='h-8 w-full'></div>
-            </div>
-            <div className="font-medium text-black text-base w-full mt-1">{room.name}</div>
-            <div className="text-gray-400 text-xs w-full">{room.bio}</div>
-            <div className="text-black text-xs flex items-center gap-1 my-2">
-                {room.is_public ? (<FaGlobeAfrica />) : (<FaLock />)}
-                {room.is_public ? "Public" : "Private"}
-            </div>
-            <div className='flex flex-col'>
-                {
-                    !room.is_public && (<div className='text-sm'>Password: {room.password}</div>)
-                }
+                <div className="font-medium text-black text-base mt-1">{room.name}</div>
+                <div className="text-gray-400 text-xs">{room.bio}</div>
+                <div className="text-black text-xs flex items-center gap-1 my-2">
+                    {room.is_public ? (<FaGlobeAfrica />) : (<FaLock />)}
+                    {room.is_public ? "Public" : "Private"}
+                </div>
+                <div className='flex flex-col'>
+                    {
+                        !room.is_public && (<div className='text-sm'>Password: {room.password}</div>)
+                    }
+                </div>
             </div>
         </div>
     )
